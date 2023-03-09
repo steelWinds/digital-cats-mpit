609744e4a0e7bbd6ce5d5f5503c9f56b8ec94760
steelWinds <kirillsurov0@gmail.com>
2023-03-10 06:17:24 +0900

    update(app): update units

---
+++ layouts/app-page.vue
@@ -0,0 +1,11 @@
+<template>
+  <el-container class="h-screen">
+    <el-main>
+      <slot />
+    </el-main>
+
+    <el-footer class="!p-0 !m-0">
+      <slot name="footer" />
+    </el-footer>
+  </el-container>
+</template>
--- pages/nfc-reader.vue
+++ pages/nfc-reader.vue
@@ -1,3 +1,41 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false
+})
+</script>
+
 <template>
-dd
+  <NuxtLayout name="app-page">
+    <article class="h-full flex justify-center items-center flex-col">
+      <nuxt-icon
+        name="nfc"
+        class="text-[150px] text-light-green animate-pulse mb-12"
+      />
+
+      <el-button
+        class="
+          !px-8
+          !py-3
+          rounded-sm
+          text-green
+          !text-lg
+          !h-auto
+        "
+      >
+        Запустить
+      </el-button>
+    </article>
+
+    <template #footer>
+      <el-row
+        justify="center"
+        align="middle"
+        class="h-full bg-green p-0 m-0"
+      >
+        <el-icon :size="32">
+          <ElIconHouse class="text-white" />
+        </el-icon>
+      </el-row>
+    </template>
+  </NuxtLayout>
 </template>
