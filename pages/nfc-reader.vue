2a791639f88d742b6622ff55b6a8f67d67852d30
steelWinds <kirillsurov0@gmail.com>
2023-03-10 11:06:13 +0900

    udpate(app): some updates

--- pages/nfc-reader.vue
+++ pages/nfc-reader.vue
@@ -36,7 +36,7 @@ const scanNDEF = async () => {
           !text-lg
           !h-auto
         "
-        @click="pending ? scanNDEF : abort"
+        @click="!pending ? scanNDEF : abort"
       >
         <span v-if="!pending">Запустить</span>
         <span v-else>Прервать</span>
